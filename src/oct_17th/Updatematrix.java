86cb6a6dced7972a0e83f9ea715242d140f48e40
zjc <1596180274@qq.com>
2021-12-20 21:52:28 +0800

    12月18号之前的code

--- src/oct_17th/Updatematrix.java
+++ src/oct_17th/Updatematrix.java
@@ -4,20 +4,54 @@ package oct_17th;
 //        两个相邻元素间的距离为 1 。
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Updatematrix {
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    public int[][] updateMatrix(int[][] mat) {
+    public static int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dist = new int[m][n];
+        boolean[][] seen = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<int[]>();
+        // 将所有的 0 添加进初始队列中
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    seen[i][j] = true;
+                }
+            }
+        }
 
-        return mat;
+        // 广度优先搜索
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int i = cell[0], j = cell[1];
+            for (int d = 0; d < 4; ++d) {
+                int ni = i + dirs[d][0];
+                int nj = j + dirs[d][1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !seen[ni][nj]) {
+                    dist[ni][nj] = dist[i][j] + 1;
+                    queue.offer(new int[]{ni, nj});
+                    seen[ni][nj] = true;
+                }
+            }
+        }
+        for (int i = 0; i <dist.length ; i++) {
+            for (int j = 0; j <dist[0].length ; j++) {
+                System.out.print(" "+dist[i][j]);
+
+            }
+            System.out.println();
+        }
+        return dist;
     }
 
     public static void main(String[] args) {
-
-
-
-
-
-
-
+        int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        updateMatrix(mat);
     }
+
 }
